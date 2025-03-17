9894ab50e7b695587fe4bf3fbd9fd51f283c84c2
13524 <13524@13524-154>
2025-03-17 17:17:40 +0900

    modified:   C practice.c

--- C practice.c
+++ C practice.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    printf("hello world\n");
+    printf("%d * %d = %d", 2, 1, 2);
+    printf("%d * %d = %d", 2, 2, 4);
+    printf("%d * %d = %d", 2, 3, 6);
+    printf("%d * %d = %d", 2, 4, 8);
+    printf("%d * %d = %d", 2, 5, 10);
+    printf("%d * %d = %d", 2, 6, 12);
+    printf("%d * %d = %d", 2, 7, 14);
+    printf("%d * %d = %d", 2, 8, 16);
+    printf("%d * %d = %d", 2, 9, 18);
     return 0;
 }
