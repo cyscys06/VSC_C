6454aa3ae08a6758e4506898934c0db9c1eb494c
cyscys06 <cyscys06@gmail.com>
2025-03-20 11:01:31 +0900

    modified:   .vscode_c/C practice.c 	modified:   .vscode_c/C practice.exe

--- .vscode_c/C practice.c
+++ .vscode_c/C practice.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    printf("%d * %d = %d\n", 2, 1, 2);
-    printf("%d * %d = %d\n", 2, 2, 4);
-    printf("%d * %d = %d\n", 2, 3, 6);
-    printf("%d * %d = %d\n", 2, 4, 8);
-    printf("%d * %d = %d\n", 2, 5, 10);
-    printf("%d * %d = %d\n", 2, 6, 12);
-    printf("%d * %d = %d\n", 2, 7, 14);
-    printf("%d * %d = %d\n", 2, 8, 16);
-    printf("%d * %d = %d\n", 2, 9, 18);
+    short x = 65;
+
+    printf("%c\n", x);
     return 0;
 }
