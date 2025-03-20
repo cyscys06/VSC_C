25eda87171dce99056d568f301ca2f58817b7e60
cyscys06 <cyscys06@gmail.com>
2025-03-20 21:25:31 +0900

    modified:   .vscode_cpp/problem 3.2.cpp

--- .vscode_cpp/problem 3.2.cpp
+++ .vscode_cpp/problem 3.2.cpp
@@ -4,11 +4,11 @@ int main()
 {
     int degree, minute, second;
     cout << "위도를 도, 분, 초 단위로 입력하시오:" << endl;
-    cout << "먼저, 도각을 입력하시오:" << endl;
+    cout << "먼저, 도각을 입력하시오:__\b\b";
     cin >> degree;
-    cout << "다음에, 분각을 입력하시오:" << endl; 
+    cout << "다음에, 분각을 입력하시오:__\b\b";
     cin >> minute;
-    cout << "끝으로, 초각을  입력하시오:" << endl;
+    cout << "끝으로, 초각을  입력하시오:__\b\b";
     cin >> second;
 
     const double weedo1 = degree;
@@ -16,4 +16,5 @@ int main()
     const double weedo3 = second / 3600;
     const double weedo = weedo1 + weedo2 + weedo3;
     cout << weedo1 << "도, " << weedo2 << "분, " << weedo3 << "초 = " << weedo << "도" << endl; 
+    return 0;
 }
