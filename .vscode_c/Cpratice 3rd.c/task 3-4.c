faebe170f29d91d7b9315ac9ee221d4f906b2493
cyscys06 <cyscys06@gmail.com>
2025-03-24 16:50:46 +0900

    new file:   .vscode_c/Cpratice 3rd.c/task 3-4.c 	new file:   .vscode_c/Cpratice 3rd.c/task 3-4.exe 	new file:   .vscode_c/Cpratice 3rd.c/task 3-5.c 	new file:   .vscode_c/Cpratice 3rd.c/task 3-5.exe 	modified:   .vscode_cpp/chapter 4/problem 4.1.cpp 	modified:   .vscode_cpp/chapter 4/problem 4.1.exe

---
+++ .vscode_c/Cpratice 3rd.c/task 3-4.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    printf("Name:\tJohn Doe\n");
+    printf("Age: \t%d\n", 30);
+    printf("Height: %f", 180.5);
+}
---
+++ .vscode_c/Cpratice 3rd.c/task 3-5.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("She said, \'C programming is fun!\'\n");
+    printf("Path: C:\\Program Files\\MyApp");
+}
