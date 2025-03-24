a506b76c735e679303a55e11eed0aa99dfbffa47
cyscys06 <cyscys06@gmail.com>
2025-03-24 13:56:48 +0900

    modified:   .vscode_cpp/chapter 4/problem 4.1.cpp 	new file:   .vscode_cpp/chapter 4/problem 4.1.exe

--- .vscode_cpp/chapter 4/problem 4.1.cpp
+++ .vscode_cpp/chapter 4/problem 4.1.cpp
@@ -1,1 +1,23 @@
 #include <iostream>
+int main()
+{
+    using namespace std;
+    char EngF[20], EngL[20];
+    char WHJ; // WHJ: 원하는 학점
+    int age;
+    cout << "영문 퍼스트 네임(이름):_\b";
+    cin.get(EngF, 20);
+    cin.get();
+    cout << "영문 라스트 네임(성):_\b";
+    cin.get(EngL, 20);
+    cin.get();
+    cout << "학생이 원하는 학점:_\b";
+    cin >> WHJ;
+    cout << "나이:_\b";
+    cin >> age;
+    
+    int num1 = WHJ;
+    int num2 = num1 + 1;
+    char HJ = num2;
+    cout << HJ << endl;
+}
