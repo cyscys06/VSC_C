2d12a1cdb5c019fd1d473b3999bf9bc4d1f3bd6c
cyscys06 <cyscys06@gmail.com>
2025-03-24 16:23:27 +0900

    renamed:    .vscode_c/C practice.exe -> .vscode_c/Cpractice 2nd.c/C practice.exe 	renamed:    .vscode_c/C practice.c -> .vscode_c/Cpractice 2nd.c/task 2-1.c 	new file:   .vscode_c/Cpratice 3rd.c/task 3-1.c     new file:   .vscode_c/Cpratice 3rd.c/task 3-1.exe 	modified:   .vscode_cpp/chapter 4/problem 4.1.cpp 	modified:   .vscode_cpp/chapter 4/problem 4.1.exe

---
+++ .vscode_c/Cpratice 3rd.c/task 3-1.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main()
+{
+    printf("%f + %f = %f", 1.5, 2.3, 3.8);
+}
--- .vscode_cpp/chapter 4/problem 4.1.cpp
+++ .vscode_cpp/chapter 4/problem 4.1.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 int main()
 {
     using namespace std;
-    char EngF[20], EngL[20];
+    string EngF, EngL;
     char WHJ; // WHJ: 원하는 학점
     int age;
     cout << "영문 퍼스트 네임(이름):_\b";
-    cin.get(EngF, 20);
+    cin >> EngF;
     cin.get();
     cout << "영문 라스트 네임(성):_\b";
-    cin.get(EngL, 20);
+    cin >> EngL;
     cin.get();
     cout << "학생이 원하는 학점:_\b";
     cin >> WHJ;
@@ -19,5 +21,11 @@ int main()
     int num1 = WHJ;
     int num2 = num1 + 1;
     char HJ = num2;
-    cout << HJ << endl;
+    
+    string EngN = EngL + EngF;
+
+    cout << "성명: " << EngL << ", " << EngF << endl;
+    cout << "학점: " << HJ << endl;
+    cout << "나이: " << age << endl;
+    return 0;
 }
