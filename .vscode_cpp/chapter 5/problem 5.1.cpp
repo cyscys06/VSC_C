72610861583a551d54711709d889befac0bd544f
cyscys06 <cyscys06@gmail.com>
2025-03-26 00:38:56 +0900

    modified:   .vscode_cpp/chapter 5/problem 5.1.cpp modified:   .vscode_cpp/chapter 5/problem 5.1.exe

--- .vscode_cpp/chapter 5/problem 5.1.cpp
+++ .vscode_cpp/chapter 5/problem 5.1.cpp
@@ -8,13 +8,11 @@ int main()
     cout << "두 번째 정수를 입력하시오: ";
     int SI; // Second Integer
     cin >> SI;
-
-    int gaesoo = SI - FI - 1;
-    int num1, num2, num3;
-    for (num1 = 0; num1 < gaesoo; num1++)
-        num2 = FI + num1;
-        num3 = num2 + num1;
-    int total = num3;
-    cout << "첫 번째, 두 번째 정수를 포함해 그 사이에 있는 모든 정수들의 합: " << num3 << endl;
+    int total;
+    
+    for (int i = FI; i <= SI; i++)
+        total = total + i;
+    
+    cout << "모든 정수들의 합: " << total << endl;
     return 0;
 }
