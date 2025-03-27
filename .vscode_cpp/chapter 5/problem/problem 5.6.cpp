aa18ec582eae83b8a01f543edf1324799bfdfdb8
cyscys06 <cyscys06@gmail.com>
2025-03-27 12:12:09 +0900

    modified:   .vscode_cpp/chapter 5/problem/problem 5.6.cpp 	modified:   .vscode_cpp/chapter 5/problem/problem 5.6.exe 	deleted:    .vscode_cpp/chapter 5/problem/s.cpp 	deleted:    .vscode_cpp/chapter 5/problem/s.exe

--- .vscode_cpp/chapter 5/problem/problem 5.6.cpp
+++ .vscode_cpp/chapter 5/problem/problem 5.6.cpp
@@ -3,18 +3,20 @@
 int main()
 {
     using namespace std;
-    
-    const int months = 12, years = 3;
-    string TOTAL[years][months];
 
-    for (int i = 0; i < 3; i++)
+    const int months = 12, years = 3;
+    string TOTAL[years][months] = 
     {
-        cout << ""
-        for ()
-    }
+        {"2025-jan","2025-feb","2025-mar","2025-apr","2025-may","2025-jun",
+        "2025-jul","2025-aug","2025-sep","2025-oct","2025-nov","2025-dec",}, // TOTAL[0]
+        {"2026-jan","2026-feb","2026-mar","2026-apr","2026-may","2026-jun",
+        "2026-jul","2026-aug","2026-sep","2026-oct","2026-nov","2026-dec",}, // TOTAL[1]
+        {"2027-jan","2027-feb","2027-mar","2027-apr","2027-may","2027-jun",
+        "2027-jul","2027-aug","2027-sep","2027-oct","2027-nov","2027-dec",}  // TOTAL[2]
+    };
 
     int num;
-    int Y[years] = {0};// 입력받은 연,월별 책 판매량을 연도별로 나눠 서로 다른 인덱스에 저장
+    int Y[years] = {0}; // 입력받은 연,월별 책 판매량을 연도별로 나눠 서로 다른 인덱스에 저장
     for(int y = 0; y < years; y++)
     {
        for(int m = 0; m < months; m++)
@@ -24,12 +26,12 @@ int main()
         Y[y] += num;
        }
     }
-
+    
     cout << "2025년 총 판매량: " << Y[0] << "권" << endl;
     cout << "2026년 총 판매량: " << Y[1] << "권" << endl;
     cout << "2027년 총 판매량: " << Y[2] << "권" << endl;
-    
-    cout << "2년간 누적 판매량: " << Y[0] + Y[1] << "권" << endl;
-    cout << "3년간 누적 판매량: " << Y[0] + Y[1] + Y[2] << "권" << endl;
+
+    cout << "2년째의 누적 판매량: " << Y[0] + Y[1]<< "권" << endl;
+    cout << "3년째의 누적 판매량: " << Y[0] + Y[1] + Y[2] << "권" << endl;
     return 0;
 }
