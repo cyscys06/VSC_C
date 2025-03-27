e62e5c858bf735ea07432db1025fee61b34d57a0
cyscys06 <cyscys06@gmail.com>
2025-03-27 15:48:14 +0900

    modified:   .vscode_cpp/chapter 5/problem/problem 5.8.cpp 	modified:   .vscode_cpp/chapter 5/problem/problem 5.8.exe new file:   .vscode_cpp/chapter 5/problem/problem 5.9.cpp 	new file:   .vscode_cpp/chapter 5/problem/problem 5.9.exe

--- .vscode_cpp/chapter 5/problem/problem 5.8.cpp
+++ .vscode_cpp/chapter 5/problem/problem 5.8.cpp
@@ -4,14 +4,18 @@ int main()
 {
     using namespace std;
     const int s = 20;
-    char voca[s];
+    char voca[s]; // 변수를 선언만 하면 처음 그 값에는 쓰레기값이 들어있다. 초기화도 해줘야 한다.
+    int num = 0; // 단어 개수
     
     cout << "영어 단어들을 입력하십시오(끝내려면 done을 입력) : " << endl;
+    cin >> voca; // voca 값 초기화(이 구문이 만약 없다면?)
+    
     while (strcmp(voca, "done")) // strcmp(배열명, 문자열): 배열에 저장된 문자열이 함수에서 지정한 문자와 같으면 0을 출력(false)
     {
-        cin >> voca;
-        cout << " ";
+        num++;
+        cin >> voca; // cin에서 공백을 만나면 자동으로 단어 하나 입력된 것으로 처리
     }
-    cout << "종료" << endl;
+    
+    cout << "총" << num << "단어가 입력되었습니다." << endl;
     return 0;
 }
