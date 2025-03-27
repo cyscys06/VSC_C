64b4cd90076e043ae66cbf4bc9069a200d56e139
cyscys06 <cyscys06@gmail.com>
2025-03-27 13:15:37 +0900

    deleted:    .vscode_cpp/chapter 4/practice.exe 	modified:   .vscode_cpp/chapter 5/problem/problem 5.7.cpp 	modified:   .vscode_cpp/chapter 5/problem/problem 5.7.exe

--- .vscode_cpp/chapter 5/problem/problem 5.7.cpp
+++ .vscode_cpp/chapter 5/problem/problem 5.7.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 int main()
+
 {
     using namespace std;
-    int ms[5] = {1, 2, 3, 4, 5};
-    
-    for (int i = 0; i < 5 i++)
+    struct car
     {
-        int total += ms[i]
+        char company[10];
+        int year;
+    };
+    int num;
+    cout << "몇 대의 차를 목록으로 관리하시겠습니까? ";
+    cin >> num;
+
+    car * cars = new car[num];
+
+    for(int i = 0; i < num; i++)
+    {
+        cout << "자동차 #" << i + 1 << ":" << endl;
+        cout << "제작업체: ";
+        cin >> cars[i].company;
+        cin.get();
+        cout << "제작년도: ";
+        cin >> cars[i].year;
+        cin.get();
     }
 
+    cout << "현재 귀하가 보유하고 있는 자동차 목록은 다음과 같습니다: " << endl;
+    
+    for (int j = 0; j < num; j++)
+    cout << cars[j].year << "년형" << cars[j].company << endl;
+    
 }
