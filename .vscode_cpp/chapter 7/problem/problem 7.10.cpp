02f9ce8c0092c34a045d35ace6efad4712528af0
cyscys06 <cyscys06@gmail.com>
2025-04-03 02:19:05 +0900

    modified:   .vscode_cpp/chapter 7/problem/problem 7.10.cpp 	modified:   .vscode_cpp/chapter 7/problem/problem 7.9.cpp 	new file:   .vscode_cpp/chapter 7/problem/problem 7.9.exe

---
+++ .vscode_cpp/chapter 7/problem/problem 7.10.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+double add(double x, double y);
+double calculate(double x, double y, add); // 함수(add)가 함수(calculate)의 매개변수가 됨
+double deducct(double x, double y);
+
+int main()
+{
+
+}
