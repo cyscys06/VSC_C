19e507d3347d40bc1e90450c66dc384a9f0c93cb
cyscys06 <cyscys06@gmail.com>
2025-04-02 22:58:41 +0900

    new file:   .vscode_cpp/chapter 7/listing/listing 7.4.cpp 	new file:   .vscode_cpp/chapter 7/listing/listing 7.4.exe renamed:    .vscode_cpp/chapter 7/listing 7.7.cpp -> .vscode_cpp/chapter 7/listing/listing 7.7.cpp 	renamed:    .vscode_cpp/chapter 7/listing 7.7.exe -> .vscode_cpp/chapter 7/listing/listing 7.7.exe 	new file:   .vscode_cpp/chapter 7/problem/problem 7.1.cpp 	new file:   .vscode_cpp/chapter 7/problem/problem 7.1.exe 	new file:   .vscode_cpp/chapter 7/problem/problem 7.10.cpp 	new file:   .vscode_cpp/chapter 7/problem/problem 7.2.cpp 	new file:   .vscode_cpp/chapter 7/problem/problem 7.2.exe 	new file:   .vscode_cpp/chapter 7/problem/problem 7.3.cpp 	new file:   .vscode_cpp/chapter 7/problem/problem 7.3.exe 	new file:   .vscode_cpp/chapter 7/problem/problem 7.4.cpp 	new file:   .vscode_cpp/chapter 7/problem/problem 7.4.exe 	new file:   .vscode_cpp/chapter 7/problem/problem 7.5.cpp 	new file:   .vscode_cpp/chapter 7/problem/problem 7.5.exe 	new file:   .vscode_cpp/chapter 7/problem/problem 7.6.cpp 	new file:   .vscode_cpp/chapter 7/problem/problem 7.6.exe 	new file:   .vscode_cpp/chapter 7/problem/problem 7.7.cpp 	new file:   .vscode_cpp/chapter 7/problem/problem 7.7.exe 	new file:   .vscode_cpp/chapter 7/problem/problem 7.8.cpp 	new file:   .vscode_cpp/chapter 7/problem/problem 7.9.cpp

---
+++ .vscode_cpp/chapter 7/problem/problem 7.1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+void calculate(double x, double y); // x, y 받아서 조화평균 계산  
+using namespace std; // 이름공간을 함수 밖에 선언해놓으면 
+                     // 함수마다 이름공간을 따로 선언하는 수고를 덜 수 있다.
+
+int main()
+{
+    
+    double first, second;
+    cout << "두 수를 입력: ";
+    while (cin >> first >> second) // first, second 둘다 입력받아야 true
+    {
+        if (first == 0 || second == 0) // 둘중 하나라도 0이면 루프 종료
+        {
+            break;
+        }
+        calculate(first, second); // 사용자 정의 함수 main함수에 사용할때는 자료형 다 빼고 함수이름, 매개변수만 넣기
+    } 
+    cout << "프로그램을 종료합니다.";
+    return 0;
+}
+
+void calculate(double x, double y) // 함수 정의(반환 안해도 됨)
+{
+    double harmony = 2.0 * x * y / (x + y); // 조화평균 계산식
+    cout << "입력한 두 수의 조화평균: " << harmony << endl;
+}
