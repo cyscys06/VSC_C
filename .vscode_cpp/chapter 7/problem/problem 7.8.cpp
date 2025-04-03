c2debb67266b79e73a55d9ba8a6ad6261c9c2ed7
cyscys06 <cyscys06@gmail.com>
2025-04-03 10:10:18 +0900

    modified:   .vscode_cpp/chapter 7/problem/problem 7.8.cpp 	new file:   .vscode_cpp/chapter 7/problem/problem 7.8.exe

---
+++ .vscode_cpp/chapter 7/problem/problem 7.8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+const int SIZE = 4;
+
+void fill()
+
+int main()
+{
+    const char * season[SIZE] = {"spring", "summer", "fall", "winter"};
+    array<double, SIZE> expense;
+}
