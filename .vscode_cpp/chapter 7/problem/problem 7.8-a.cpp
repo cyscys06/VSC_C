cbb14d7a60574806970f98696df0b09c1986f4da
cyscys06 <cyscys06@gmail.com>
2025-04-03 17:23:38 +0900

    modified:   .vscode_cpp/chapter 7/problem/problem 7.3.exe 	modified:   .vscode_cpp/chapter 7/problem/problem 7.8-a.cpp 	new file:   .vscode_cpp/chapter 7/problem/problem 7.8-a.exe 	modified:   .vscode_cpp/chapter 7/problem/problem 7.8-b.cpp 	new file:   .vscode_cpp/chapter 7/problem/problem 7.8-b.exe

--- .vscode_cpp/chapter 7/problem/problem 7.8-a.cpp
+++ .vscode_cpp/chapter 7/problem/problem 7.8-a.cpp
@@ -1,40 +1,34 @@
 #include <iostream>
-#include <string>
+#include <array>
+using namespace std;
 
+const int Seasons = 4; // 배열크기
+const char* Snames[Seasons] = {"Spring", "Summer", "Fall", "Winter"}; // 사계절을 원소로 가지는 char 포인터형 배열 Sname 선언
 
-const int Seasons = 4;
-
-const char* Snames[Seasons] =
-{ "Spring", "Summer", "Fall", "Winter" };
-
-// array 객체를 수정하는 기능
-void fill(double pa[]);
-// 수정하지 않고 객체를 사용하는 기능
-void show(double da[]);
+void fill(array<double, Seasons> &pa); // pa가 매개변수로 오는 array객체를 참조(데이터 복사 X) -> 직접 입력 받아야 하므로 const 사용 X
+void show(const array<double, Seasons> &da); // 출력만 하므로 값을 변경하지 않도록 const로 고정
 
 int main()
 {
-    double expenses[Seasons];
-    fill(expenses);
+    array<double, Seasons> expenses; // 크기가 expenses이고 자료형이 double인 array객체 expenses 선언
+    fill(expenses); // 매개변수로 array객체 expenses를 사용해 함수 호출
     show(expenses);
     return 0;
 }
 
-void fill(double pa[])
+void fill(array<double, Seasons> &pa) // 계절별 소모 비용 입력
 {
-    using namespace std;
     for (int i = 0; i < Seasons; i++)
     {
-        cout << Snames[i] << "에 소요되는 비용:";
+        cout << Snames[i] << "에 소요되는 비용: ";
         cin >> pa[i];
     }
 }
 
-void show(double da[])
+void show(const array<double, Seasons> &da) // 계절별 소모 비용 및 사계절 총 소모 비용 출력
 {
-    using namespace std;
     double total = 0.0;
-    cout << "\n계절별비용\n";
+    cout << "\n계절별 비용\n";
     for (int i = 0; i < Seasons; i++)
     {
         cout << Snames[i] << ": $" << da[i] << endl;
--- .vscode_cpp/chapter 7/problem/problem 7.8-b.cpp
+++ .vscode_cpp/chapter 7/problem/problem 7.8-b.cpp
@@ -1,41 +1,43 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 const int Seasons = 4;
-const char* Snames[Seasons] = { "Spring", "Summer", "Fall", "Winter" };
+const char* Snames[Seasons] = {"Spring", "Summer", "Fall", "Winter"};
 
-struct S_exp {
-    double expenses[Seasons];
+struct S_exp // S_exp라는 구조체 선언
+{
+    double expenses[Seasons]; // 크기가 Seasons인 double형 배열 expenses 선언
 };
 
-void fill(struct S_exp *);
-void show(struct S_exp);
+void fill(S_exp * pa); // S-exp 포인터형 변수 pa를 매개변수로 하는 함수 선언(그냥 pa: 데이터 복사, * pa: 원본 참조)
+void show(S_exp da);
 
-
-int main() {
+int main() 
+{
     S_exp new_exp = {};
     fill(&new_exp);
     show(new_exp);
     return 0;
 }
 
-
-void fill(struct S_exp* pa) {
-    for (int i = 0; i < Seasons; i++) {
+void fill(S_exp* pa) // 입력
+{
+    for (int i = 0; i < Seasons; i++) 
+    {
         cout << Snames[i] << "에 소요되는 비용: ";
         cin >> pa->expenses[i];
     }
 }
 
-
-void show(struct S_exp da) {
+void show(S_exp da) // 출력
+{
     double total = 0.0;
     cout << "\n계절별 비용\n";
-    for (int i = 0; i < Seasons; i++) {
-        cout << Snames[i] << ": $" << da.expenses[i] << '\n';
+    for (int i = 0; i < Seasons; i++) 
+    {
+        cout << Snames[i] << ": " << da.expenses[i] << '\n';
         total += da.expenses[i];
     }
-    cout << "총비용: $" << total << '\n';
+    cout << "총비용: " << total << '\n';
 }
