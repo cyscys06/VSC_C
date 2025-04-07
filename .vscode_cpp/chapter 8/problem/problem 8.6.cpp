261b8a482dc29f0d0ac2602da6d4c74a8ccd12db
cyscys06 <cyscys06@gmail.com>
2025-04-07 15:15:59 +0900

    modified:   .vscode_cpp/chapter 8/problem/problem 8.4.cpp 	modified:   .vscode_cpp/chapter 8/problem/problem 8.5.cpp 	modified:   .vscode_cpp/chapter 8/problem/problem 8.5.exe 	modified:   .vscode_cpp/chapter 8/problem/problem 8.6.cpp 	modified:   .vscode_cpp/chapter 8/problem/problem 8.6.exe

--- .vscode_cpp/chapter 8/problem/problem 8.6.cpp
+++ .vscode_cpp/chapter 8/problem/problem 8.6.cpp
@@ -2,50 +2,51 @@
 #include <cstring>  // strlen 사용
 using namespace std;
 
-// 일반 템플릿 함수: T형 배열에서 최댓값 반환
-template <typename T>
-T maxn(T arr[], int n) {
+template <typename T> T maxn(T arr[], int n); // 첫번째 매개변수: 배열명 두번째 매개변수: 배열크기
+template <> const char * maxn(const char * arr[], int n);
+
+int main() 
+{
+    int integer[6] = {2, 8, 5, 1, 10, 3};
+    cout << "int 배열 최댓값: " << maxn(integer, 6) << endl;
+
+    double demical[4] = {2.5, 9.3, 4.4, 7.1};
+    cout << "double 배열 최댓값: " << maxn(demical, 4) << endl;
+
+    const char * strings[5] = // char*형 문자열
+    {
+        "apple",
+        "strawberry", // 가장 긴 문자열
+        "banana",
+        "grape",
+        "watermelon"
+    };
+    cout << "가장 긴 문자열: " << maxn(strings, 5) << endl;
+    return 0;
+}
+
+template <typename T> T maxn(T arr[], int n) // 템플릿함수(int, double형 가능)
+{
     T max = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > max) {
+    for (int i = 1; i < n; i++) 
+    {
+        if (arr[i] > max) 
+        {
             max = arr[i];
         }
     }
     return max;
 }
 
-// 특수화된 템플릿 함수: char* 배열에서 가장 긴 문자열의 주소 반환
-template <>
-const char* maxn(const char* arr[], int n) {
-    const char* longest = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (strlen(arr[i]) > strlen(longest)) {
+template <> const char * maxn(const char * arr[], int n) // 특수화 함수(char*일때만 해당)
+{
+    const char * longest = arr[0];
+    for (int i = 1; i < n; i++) 
+    {
+        if (strlen(arr[i]) > strlen(longest)) 
+        {
             longest = arr[i];
         }
     }
     return longest;
-}
-
-int main() {
-    // int 배열 테스트
-    int intArr[6] = {2, 8, 5, 1, 10, 3};
-    cout << "int 배열 최댓값: " << maxn(intArr, 6) << endl;
-
-    // double 배열 테스트
-    double doubleArr[4] = {2.5, 9.3, 4.4, 7.1};
-    cout << "double 배열 최댓값: " << maxn(doubleArr, 4) << endl;
-
-    // 문자열 배열 테스트
-    const char* strings[5] = {
-        "apple",
-        "strawberry",
-        "banana",
-        "grape",
-        "watermelon"
-    };
-
-    const char* longestStr = maxn(strings, 5);
-    cout << "가장 긴 문자열: " << longestStr << endl;
-
-    return 0;
 }
