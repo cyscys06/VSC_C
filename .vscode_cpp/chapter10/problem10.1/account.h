f91566480361af732a0f95e0dc101064abf9bf8f
cyscys06 <cyscys06@gmail.com>
2025-04-15 12:41:49 +0900

    modified:   .vscode_cpp/chapter10/problem10.1/account.h 	modified:   .vscode_cpp/chapter10/problem10.1/problem10.1.exe 	modified:   .vscode_cpp/chapter10/problem10.1/useaccnt.cpp

--- .vscode_cpp/chapter10/problem10.1/account.h
+++ .vscode_cpp/chapter10/problem10.1/account.h
@@ -12,7 +12,6 @@ private:
     public:
     Account(); // 생성자
     Account(const std::string a, std::string b, double c); // 객체 초기화
-    ~Account(); // 파괴자
     void output(); // 정보출력
     void ipgeum(double money); // 계좌에 돈 액수(매개변수) 입금
     void choolgeum(double money); // 계좌에 돈 액수(매개변수) 출금
