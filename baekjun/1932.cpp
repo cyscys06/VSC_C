694188c845dbd351886b793d9680fca6b15eb66a
cyscys06 <cyscys06@gmail.com>
2025-08-06 16:29:49 +0900

    new file:   baekjun/.cph/.1932.cpp_6bd1846f04757c8c209d4e7dc2d5a835.prob 	new file:   baekjun/11053.cpp 	new file:   baekjun/1932.cpp 	modified:   programmers/.cph/.donut.cpp_dfaa5fb57d9ad039ab52156eabb2ade4.prob 	modified:   programmers/school.cpp 	modified:   programmers/triangle.cpp

--- baekjun/1932.cpp
+++ baekjun/1932.cpp
@@ -1,7 +1,28 @@
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+int solution(vector<vector<int>> triangle);
+
+int main()
+{
+    int N;
+    cin >> N;
+    vector<vector<int>> triangle(N);
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            int num;
+            cin >> num;
+            triangle[i - 1].push_back(num);
+        }
+    }
+
+    cout << solution(triangle);
+    return 0;
+}
 
 int solution(vector<vector<int>> triangle)
 {
--- programmers/school.cpp
+++ programmers/school.cpp
@@ -2,28 +2,55 @@
 #include <vector>
 
 using namespace std;
+const int M = 1000000007; // 루프 반복할때마다 M으로 나눠줘서 혹여 M보다 큰 경우의수 나오는 경우에 대비
 
-int solution(int m, int n, vector<vector<int>> puddles) // puddles 크기는 기본적으로 m, n이고 물웅덩이 좌표까지 담겨있음
+int solution(int m, int n, vector<vector<int>> puddles) // puddles 에는 물웅덩이 좌표만 담겨있음 -> 좌표 저장하는 2차원 벡터 별도로 만들기
 {
     int answer = 0;
+    vector<vector<int>> coordinate(n, vector<int>(m, 0)); // 격자 좌표 벡터
+    vector<vector<bool>> isPuddle(n, vector<bool>(m, false)); // 물웅덩이 좌표 기록용 플래그 생성
     
-    // puddles 인덱스값 모두 0으로 초기화
+    // puddles 실제좌표값에서 1씩 빼주기(격자용 좌표와 비교하기 위해)
     for (int i = 0; i < puddles.size(); i++)
     {
-        for (int j = 0; j < puddles[i].size(); j++)
-        {
-            puddles[i][j] = 0;
-        }
+        puddles[i][0] -= 1;
+        puddles[i][1] -= 1;
+        isPuddle[puddles[i][1]][puddles[i][0]] = true; // 물웅덩이가 있는 좌표는 true로 기록
     }
 
     // 현재 좌표 인덱스 = 이전 좌표들(현재좌표 기준 위쪽과 왼쪽에 있는 좌표들)의 인덱스 값들을 더한 것
-    for (int i = 0; i < puddles.size(); i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < puddles[i].size(); j++)
+        for (int j = 0; j < m; j++)
         {
+            if (isPuddle[i][j]) // 현재 좌표가 물웅덩이면
+            {
+                continue; // 강제로 다음 루프로 넘어가기
+            } 
+            // 물웅덩이인지를 먼저 체크해주기
             
+            if (i == 0 && j == 0) // 시작지점
+            {
+                coordinate[i][j] = 1;
+            }
+            else if (i > 0 && j == 0) // 현재좌표 기준 위쪽좌표만 존재
+            {
+                coordinate[i][j] += coordinate[i - 1][j];
+            }
+            else if (i == 0 && j > 0) // 현재좌표 기준 왼쪽좌표만 존재
+            {
+                coordinate[i][j] += coordinate[i][j - 1];
+            }
+            else // 현재좌표 기준 위쪽, 왼쪽좌표 둘다 존재
+            {
+                coordinate[i][j] += (coordinate[i - 1][j] + coordinate[i][j - 1]);
+            }
+
+            coordinate[i][j] %= M; // 루프마다 M으로 나눠서 M보다 큰수 사라지는 현상 방지
         }
     }
+
+    answer = coordinate[n - 1][m - 1];
 
     return answer;
 }
