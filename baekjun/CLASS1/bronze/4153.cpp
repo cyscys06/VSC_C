4860fc07f61c7cb2917033c5c8ce424102eb4560
cyscys06 <cyscys06@gmail.com>
2025-09-09 20:11:18 +0900

    modified:   baekjun/CLASS1/bronze/4153.cpp

--- baekjun/CLASS1/bronze/4153.cpp
+++ baekjun/CLASS1/bronze/4153.cpp
@@ -3,15 +3,50 @@ using std::cout;
 using std::cin;
 using std::vector;
 
+bool isright(int a, int b, int c);
+
 int main()
 {
-    vector<vector<int>> T;
-
-    int a;
-    while (cin >> a)
+    int a, b, c;
+    vector<vector<int>> A;
+    
+    while (cin >> a >> b >> c) 
     {
-        vector<int> row;
+        if (a == 0) // 0 입력받으면 입력 종료
+        {
+            break;
+        }
 
-        for (int i = 0; i < )
+        A.push_back({a, b, c}); 
+        
     }
+
+    for (int i = 0; i < A.size(); i++)
+    {
+        sort(A[i].begin(), A[i].end()); // 검사 전 정렬
+
+        if (isright(A[i][0], A[i][1], A[i][2]))
+        {
+            cout << "right";
+        }
+        else
+        {
+            cout << "wrong";
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
+
+bool isright(int a, int b, int c)
+{
+    if ((a * a) + (b * b) == (c * c))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    } 
 }
