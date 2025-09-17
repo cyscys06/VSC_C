fbdea8e0d7af6cc225500bd7ebcd8700b98d7a0e
cyscys06 <cyscys06@gmail.com>
2025-09-17 20:14:42 +0900

    new file:   asdasd.cpp 	new file:   asdasd.exe 	new file:   baekjun/Marathon/Marathon3/.cph/.25943.cpp_c08bd222ea44eb37fb02745d1225ea40.prob 	new file:   baekjun/Marathon/Marathon3/25943.cpp

---
+++ asdasd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "첫번째 정수: ";
+    cin >> a;
+
+    cout << "두번째 정수: ";
+    cin >> b;
+    cout << "결과: " << a + b;
+    return 0;
+}
